b743fb4ae28f9d46725cabf46058c689389b644f
Umama9000 <121767388+Umama9000@users.noreply.github.com>
2024-12-26 06:53:43 +0000

    new work

---
+++ Datatype.js
@@ -0,0 +1,15 @@
+let a = 123 // Number
+let b = "Umama" //String
+let c = true //Boolean
+let d = null //Object
+let e = undefined //undefined
+
+
+
+
+
+
+
+
+
+console.log(typeof d)
---
+++ varaible.js
@@ -0,0 +1,5 @@
+var accountNumb = 22351;
+const accountEmail = 88889999;
+let accountId = 55333;
+
+console.log({accountNumb,accountEmail,accountId});
