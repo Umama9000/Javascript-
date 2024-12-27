27b0c1e1c60cd14d73945df3551374c095378dca
Umama9000 <121767388+Umama9000@users.noreply.github.com>
2024-12-27 04:17:39 +0000

    Work on operators

---
+++ operation.js
@@ -0,0 +1,19 @@
+let num = 99
+let negNum = -num
+console.log(negNum);
+
+let gameCounte = 99;
+gameCounte ++;
+console.log(gameCounte);
+
+
+console.log(+true);
+console.log(+false);
+console.log("1" +8 * 2);
+console.log(9*8 + "9");
+
+
+let stri1 = "Umama ";
+let stri2 = "Hello";
+let stri3 = stri1 + stri2;
+console.log(stri3);
