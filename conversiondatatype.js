7dff7c60ce7d730bdf5a6261f216369f7b73955d
Umama9000 <121767388+Umama9000@users.noreply.github.com>
2024-12-27 03:15:26 +0000

    conversion data type

---
+++ conversiondatatype.js
@@ -0,0 +1,12 @@
+// let someNon = 99;
+// let StringNo = String(someNon);
+// console.log(StringNo);
+// console.log(typeof StringNo);
+
+let someNo = "one99";
+let valueInNo = Number(someNo);
+console.log(valueInNo);
+console.log(typeof valueInNo);
+console.log(someNo);
+
+
